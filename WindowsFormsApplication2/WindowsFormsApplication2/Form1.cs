f393c44c519dff8a49ff777a94cdef8229ecd54d
Alex-cpu-debag <61779810+Alex-cpu-debag@users.noreply.github.com>
2020-05-18 10:49:32 +0300

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+using hashing;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Directory.CreateDirectory(@"C:\" + textBox2.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message);
+            }
+
+            string path = @"C:\" + textBox2.Text + @"\MD5.txt";
+            string path1 = @"C:\" + textBox2.Text + @"\SHA256.txt";
+            string path2 = @"C:\" + textBox2.Text + @"\SHA384.txt";
+            string path3 = @"C:\" + textBox2.Text + @"\SHA512.txt";
+            string path4 = @"C:\" + textBox2.Text + @"\SHA256S.txt";
+            string path5 = @"C:\" + textBox2.Text + @"\SHA384S.txt";
+            string path6 = @"C:\" + textBox2.Text + @"\SHA512S.txt";
+
+            using (StreamWriter stream = new StreamWriter(path, true))
+            {
+                stream.WriteLine("" + textBox10.Text);
+            }
+            using (StreamWriter stream = new StreamWriter(path1, true))
+            {
+                stream.WriteLine("" + textBox9.Text);
+            }
+            using (StreamWriter stream = new StreamWriter(path2, true))
+            {
+                stream.WriteLine("" + textBox8.Text);
+            }
+            using (StreamWriter stream = new StreamWriter(path3, true))
+            {
+                stream.WriteLine("" + textBox7.Text);
+            }
+            using (StreamWriter stream = new StreamWriter(path4, true))
+            {
+                stream.WriteLine("" + textBox6.Text);
+            }
+            using (StreamWriter stream = new StreamWriter(path5, true))
+            {
+                stream.WriteLine("" + textBox5.Text);
+            }
+            using (StreamWriter stream = new StreamWriter(path6, true))
+            {
+                stream.WriteLine("" + textBox4.Text);
+            }
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            textBox6.Text = Hashing.ComputeHash(textBox2.Text, Supported_HA.SHA256, null);
+            textBox5.Text = Hashing.ComputeHash(textBox2.Text, Supported_HA.SHA384, null);
+            textBox4.Text = Hashing.ComputeHash(textBox2.Text, Supported_HA.SHA512, null);
+            textBox9.Text = Hashing.ComputeHashNoSalt(textBox2.Text, Supported_HA.SHA256);
+            textBox8.Text = Hashing.ComputeHashNoSalt(textBox2.Text, Supported_HA.SHA384);
+            textBox7.Text = Hashing.ComputeHashNoSalt(textBox2.Text, Supported_HA.SHA512);
+            textBox10.Text = Hashing.CalculateMD5Hash(textBox2.Text, Supported_HA.MD5);
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+         
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+        }
+
+        private void выбратьФайлToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dialog = new OpenFileDialog())// создание нового OpenFileDialog
+            {
+                dialog.Filter = "Текстовые файлы|*.txt"; // отображение файлов с расширением .txt
+                if (dialog.ShowDialog() == DialogResult.OK) // если выбираем файл и нажимаем кнопку ОК
+                {
+                    textBox3.Text = ("SHA256: ");
+                    textBox3.Text = Hashing.ComputeHashNoSalt(File.ReadAllText(dialog.FileName), Supported_HA.SHA256);
+                    textBox3.Text = Hashing.ComputeHashNoSalt(File.ReadAllText(dialog.FileName), Supported_HA.SHA384);
+                     // вывод хеша в текст бокс файла, 
+                    // предварительно считав с него текст                    
+                }
+            }
+        }
+    }
+}
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Hash.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace hashing
+{
+    public enum Supported_HA
+    {
+        SHA256, SHA384, SHA512, MD5
+    }
+
+    class Hashing
+    {
+        public static string ComputeHash(string plainText, Supported_HA hash, byte[] salt)
+        {
+            int minSaltLength = 4, maxSaltLength = 16;
+
+            byte[] SaltBytes = null;
+            if (salt != null)
+            {
+                SaltBytes = salt;
+            }
+            else
+            {
+                Random r = new Random();
+                int SaltLength = r.Next(minSaltLength, maxSaltLength);
+                SaltBytes = new byte[SaltLength];
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                rng.GetNonZeroBytes(SaltBytes);
+                rng.Dispose();
+            }
+
+
+            byte[] plainData = ASCIIEncoding.UTF8.GetBytes(plainText);
+            byte[] plainDataWithSalt = new byte[plainData.Length + SaltBytes.Length];
+
+            for (int x = 0; x < plainData.Length; x++)
+                plainDataWithSalt[x] = plainData[x];
+            for (int n = 0; n < SaltBytes.Length; n++)
+                plainDataWithSalt[plainData.Length + n] = SaltBytes[n];
+
+            byte[] hashValue = null;
+
+            switch (hash)
+            {
+                case Supported_HA.SHA256:
+                    SHA256Managed sha = new SHA256Managed();
+                    hashValue = sha.ComputeHash(plainDataWithSalt);
+                    sha.Dispose();
+                    break;
+                case Supported_HA.SHA384:
+                    SHA384Managed sha1 = new SHA384Managed();
+                    hashValue = sha1.ComputeHash(plainDataWithSalt);
+                    sha1.Dispose();
+                    break;
+                case Supported_HA.SHA512:
+                    SHA512Managed sha2 = new SHA512Managed();
+                    hashValue = sha2.ComputeHash(plainDataWithSalt);
+                    sha2.Dispose();
+                    break;
+            }
+
+            byte[] result = new byte[hashValue.Length + SaltBytes.Length];
+            for (int x = 0; x < hashValue.Length; x++)
+                result[x] = hashValue[x];
+            for (int n = 0; n < SaltBytes.Length; n++)
+                result[hashValue.Length + n] = SaltBytes[n];
+
+            return Convert.ToBase64String(result);
+        }
+        /// <summary>
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="hash"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
+        public static string ComputeHashNoSalt(string plainText, Supported_HA hash)
+        {
+            byte[] plainData = ASCIIEncoding.UTF8.GetBytes(plainText);
+
+            for (int x = 0; x < plainData.Length; x++) ;
+
+
+            byte[] hashValue = null;
+
+            switch (hash)
+            {
+                case Supported_HA.SHA256:
+                    SHA256Managed sha = new SHA256Managed();
+                    hashValue = sha.ComputeHash(plainData);
+                    sha.Dispose();
+                    break;
+                case Supported_HA.SHA384:
+                    SHA384Managed sha1 = new SHA384Managed();
+                    hashValue = sha1.ComputeHash(plainData);
+                    sha1.Dispose();
+                    break;
+                case Supported_HA.SHA512:
+                    SHA512Managed sha2 = new SHA512Managed();
+                    hashValue = sha2.ComputeHash(plainData);
+                    sha2.Dispose();
+                    break;
+            }
+
+            byte[] result = new byte[hashValue.Length];
+            for (int x = 0; x < hashValue.Length; x++)
+                result[x] = hashValue[x];
+
+            return Convert.ToBase64String(result);
+        }
+        /// <summary>
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <param name="hashValue"></param>
+        /// <param name="hash"></param>
+        /// <returns></returns>
+
+        public static bool Confirm(string plainText, string hashValue, Supported_HA hash)
+        {
+            byte[] hashBytes = Convert.FromBase64String(hashValue);
+
+            int hashSize = 0;
+
+            switch (hash)
+            {
+                case Supported_HA.SHA256:
+                    hashSize = 32;
+                    break;
+                case Supported_HA.SHA384:
+                    hashSize = 48;
+                    break;
+                case Supported_HA.SHA512:
+                    hashSize = 64;
+                    break;
+            }
+
+            byte[] SaltBytes = new byte[hashBytes.Length - hashSize];
+
+            for (int x = 0; x < SaltBytes.Length; x++)
+                SaltBytes[x] = hashBytes[hashSize + x];
+
+            string newHash = ComputeHash(plainText, hash, SaltBytes);
+
+            return (hashValue == newHash);
+        }
+      /*  public static string ComputeHashMD5(string plainText, Supported_HA hash)
+        {	
+          // создаем объект этого класса. Отмечу, что он создается не через new, а вызовом метода Create
+          MD5 md5Hasher = MD5.Create();
+          // Преобразуем входную строку в массив байт и вычисляем хэш
+          byte[] data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(input));
+          // Создаем новый Stringbuilder (Изменяемую строку) для набора байт
+          StringBuilder sBuilder = new StringBuilder();
+          // Преобразуем каждый байт хэша в шестнадцатеричную строку
+          for (int i = 0; i < data.Length; i++)
+          {    
+              //указывает, что нужно преобразовать элемент в шестнадцатиричную строку длиной в два символа    
+              sBuilder.Append(data[i].ToString("x2"));
+          }          
+          sBuilder.ToString();        
+          string pasHash = getMd5Hash("Hash");
+        }
+        */
+
+        public static string CalculateMD5Hash(string input, Supported_HA hash)
+        {
+
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashh = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashh.Length; i++)
+            {
+                sb.Append(hashh[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+        
+    }
+    /// <summary>
+    ///
+    /// </summary>
+  /*  public class HMACSHA512example
+    {
+
+        public static void file(string[] Fileargs)
+        {
+            string dataFile;
+            string signedFile;
+            //Если имена файлов не указаны, создать их.
+            if (Fileargs.Length < 2)
+            {
+                dataFile = @"text.txt";
+                signedFile = "signedFile.enc";
+
+                if (!File.Exists(dataFile))
+                {
+                    // Создайте файл для записи.
+                    using (StreamWriter sw = File.CreateText(dataFile))
+                    {
+                        sw.WriteLine("Вот сообщение, чтобы подписать");
+                    }
+                }
+            }
+            else
+            {
+                dataFile = Fileargs[0];
+                signedFile = Fileargs[1];
+            }
+            try
+            {
+                //Создайте случайный ключ с помощью генератора случайных чисел. Это было бы
+                //секретный ключ, совместно с отправителем и получателем.
+                byte[] secretkey = new Byte[64];
+                //RNGCryptoServiceProvider является реализацией генератора случайных чисел.
+                using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+                {
+                    // Массив теперь заполнен криптографически сильными случайными байтами.
+                    rng.GetBytes(secretkey);
+
+                    // Используйте секретный ключ, чтобы подписать файл сообщения.
+                    SignFile(secretkey, dataFile, signedFile);
+
+                    // Проверить подписанный файл
+                    VerifyFile(secretkey, signedFile);
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ошибка: Файл не найден", e);
+            }
+        }  //end main
+        // Вычисляет хэш с ключом для исходного файла и создает целевой файл с помощью хэша с ключом
+        // к содержимому исходного файла. 
+        public static void SignFile(byte[] key, String sourceFile, String destFile)
+        {
+            // Инициализовать объект хэша с ключом.
+            using (HMACSHA512 hmac = new HMACSHA512(key))
+            {
+                using (FileStream inStream = new FileStream(sourceFile, FileMode.Open))
+                {
+                    using (FileStream outStream = new FileStream(destFile, FileMode.Create))
+                    {
+                        // Вычислите хэш вхотового файла.
+                        byte[] hashValue = hmac.ComputeHash(inStream);
+                        // Сбросить inStream к началу файла.
+                        inStream.Position = 0;
+                        // Напишите вычисленный хэш-значение в выводной файл.
+                        outStream.Write(hashValue, 0, hashValue.Length);
+                        // Копируйте содержимое sourceFile к destFile.
+                        int bytesRead;
+                        // читать 1K за один раз
+                        byte[] buffer = new byte[1024];
+                        do
+                        {
+                            // Читайте с обертывания CryptoStream.
+                            bytesRead = inStream.Read(buffer, 0, 1024);
+                            outStream.Write(buffer, 0, bytesRead);
+                        } while (bytesRead > 0);
+                    }
+                }
+            }
+            return;
+        } // end SignFile
+
+        // Сравнивает ключ в исходном файле с новым ключом, созданным для части файла данных. Если ключи
+        // сравнить данные не были подделаны.
+        public static bool VerifyFile(byte[] key, String sourceFile)
+        {
+            bool err = false;
+            // Инициализовать объект хэша с ключом.
+            using (HMACSHA512 hmac = new HMACSHA512(key))
+            {
+                // Создайте массив для хранения считываемого значения хэша с ключами из файла.
+                byte[] storedHash = new byte[hmac.HashSize / 8];
+                // Создайте ФайлСтрим для исходного файла.
+                using (FileStream inStream = new FileStream(sourceFile, FileMode.Open))
+                {
+                    // Читает данные в storedHash.
+                    inStream.Read(storedHash, 0, storedHash.Length);
+                    // Вычислите хэш оставшегося содержимого файла.
+                    // Поток правильно расположен в начале содержимого,
+                    // сразу после сохраненного значения хэша.
+                    byte[] computedHash = hmac.ComputeHash(inStream);
+                    // сравнить вычисленный хэш с сохраненным значением
+
+                    for (int i = 0; i < storedHash.Length; i++)
+                    {
+                        if (computedHash[i] != storedHash[i])
+                        {
+                            err = true;
+                        }
+                    }
+                }
+            }
+            if (err)
+            {
+                Console.WriteLine("Хэш значения отличаются! Подписанный файл был подделан!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Хэш значения согласованы - фальсификации не произошло.");
+                return true;
+
+            } //end VerifyFile
+        } //end class
+        
+    }*/
+ 
+}
